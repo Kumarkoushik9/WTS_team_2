0ede7ce7e831ddc7ad055a8d43ca5d6c5b5d22ae
Chambeti, Teja Sri <tejcham@iu.edu>
2023-10-15 18:48:44 -0400

    added roles dropdown

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -15,30 +15,51 @@ import {Router} from '@angular/router';
     
     SignUpForm!: FormGroup;
     isSignUp:any=false;
+    roles:any =['Admin','Customer']
+    role: any='';
+
     constructor(private loginservice : LoginService,private router: Router){
       this.isSignUp==false
+      this.SignUpForm = new FormGroup({
+        'fullname': new FormControl( null),
+        'email': new FormControl(null),
+        'username':new FormControl(null),
+        'password':new FormControl(null),
+        role:new FormControl(null)
+
+      });
+      this.SignUpForm.get('role')?.setValue("")
       
     }
     ngOnInit() {
-      this.SignUpForm = new FormGroup({
-          'fullname': new FormControl( null),
-          'email': new FormControl(null),
-          'username':new FormControl(null),
-          'password':new FormControl(null),
+      // this.SignUpForm = new FormGroup({
+      //     'fullname': new FormControl( null),
+      //     'email': new FormControl(null),
+      //     'username':new FormControl(null),
+      //     'password':new FormControl(null),
+      //     role:new FormControl(null)
 
-        });
+      //   });
+      //   this.SignUpForm.get('role')?.setValue("")
       }
     // onSubmit() {
         // this.isSignUp=true
     // }
+
+    changeRole(e:any) {
+      // console.log(e.target.value)
+      this.SignUpForm.get('role')?.setValue(e.target.value)
+    }
+
     onSubmit(){
       var name=this.SignUpForm?.get('fullname') as FormControl;
       var email=this.SignUpForm?.get('email') as FormControl;
       var username=this.SignUpForm?.get('username') as FormControl;
       var password=this.SignUpForm?.get('password') as FormControl;
+      var role = this.SignUpForm?.get('role') as FormControl;
 
       this.isSignUp=true
-      var input_data={name:name.value,email:email.value,username:username.value,password:password.value}
+      var input_data={name:name.value,email:email.value,username:username.value,password:password.value,role:role.value}
       console.log(input_data)
       this.loginservice.create_account(input_data)
       .subscribe((data: any) => {
