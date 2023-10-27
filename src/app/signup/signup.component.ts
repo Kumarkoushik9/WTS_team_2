fff566bb2c3afc4fc0e0f52ae320db0cecc8492e
aarcjackpy <aarcjack@iu.edu>
2023-10-27 14:14:09 -0400

    sign up api working

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -4,9 +4,7 @@ import { MatIconModule } from "@angular/material/icon";
 import { RouterLink } from "@angular/router";
 import { FormBuilder, FormControl, FormGroup, Validators,ReactiveFormsModule } from "@angular/forms";
 import {Router} from '@angular/router';
-import { LoginService } from "../services/login_service";
-
-
+import { HttpClient } from "@angular/common/http";
 
 @Component({
     selector: 'app-login',
@@ -18,21 +16,18 @@ import { LoginService } from "../services/login_service";
     
     SignUpForm!: FormGroup;
     isSignUp:any=false;
-
-    // roles:any =['Admin','Customer']
+    roles:any =['Professional','Client']
     role: any='';
 
-    constructor(private router: Router, private fb: FormBuilder){
-
-   
+    constructor(private router: Router, private httpClient: HttpClient){
       this.isSignUp==false
-      this.SignUpForm = this.fb.group({
-        'fullname': ['',Validators.required],
-        'email': [''],
-        'username':[''],
-        'password':[''],
-
-        // role:new FormControl(null)
+      this.SignUpForm = new FormGroup({
+        'fullname': new FormControl( null),
+        'email': new FormControl(null),
+        'username':new FormControl(null),
+        'password':new FormControl(null),
+        'cPassword':new FormControl(null),
+        role:new FormControl(null)
 
       });
       // this.SignUpForm.get('role')?.setValue("")
@@ -63,19 +58,19 @@ import { LoginService } from "../services/login_service";
       var email=this.SignUpForm?.get('email') as FormControl;
       var username=this.SignUpForm?.get('username') as FormControl;
       var password=this.SignUpForm?.get('password') as FormControl;
-
-      // var role = this.SignUpForm?.get('role') as FormControl;
+      var cPassword=this.SignUpForm?.get('cPassword') as FormControl;
+      var role = this.SignUpForm?.get('role') as FormControl;
 
       this.isSignUp=true
-      var input_data={name:name.value,email:email.value,username:username.value,password:password.value}
+      var input_data={name:name.value,email:email.value,username:username.value,password:password.value,cPassword:cPassword.value,role:role.value}
+      console.log(typeof name)
       console.log(input_data)
+      return this.httpClient.post('http://127.0.0.1:5000/insert', input_data)
       // this.loginservice.create_account(input_data)
-      // .subscribe((data: any) => {
-      //     console.log(data)
+      .subscribe((data: any) => {
+          console.log(data)
           // this.router.navigate(['/login']);
-      // });
-      this.router.navigate(['/login']);
-     
+      });
   
     }
 
